db4d2667cafb30fd1bd1793b09b6a5120955dec2
Cibele Carneiro <32349643+ccarneiro-ti@users.noreply.github.com>
2017-11-28 23:16:43 -0300

    Adcionando o enunciado da questao

--- Resolucoes-URI/URI 1010- Calculo Simples.py
+++ Resolucoes-URI/URI 1010- Calculo Simples.py
@@ -1,3 +1,15 @@
+'''
+Neste problema, deve-se ler o código de uma peça 1, o número de peças 1, o valor unitário de cada peça 1, o código de uma peça 2, o número de peças 2 e o valor unitário de cada peça 2. Após, calcule e mostre o valor a ser pago.
+
+Entrada
+
+O arquivo de entrada contém duas linhas de dados. Em cada linha haverá 3 valores, respectivamente dois inteiros e um valor com 2 casas decimais.
+
+Saída
+
+A saída deverá ser uma mensagem conforme o exemplo fornecido abaixo, lembrando de deixar um espaço após os dois pontos e um espaço após o "R$". O valor deverá ser apresentado com 2 casas após o ponto.
+'''
+
 n_peca1,quant1,v_peca1 = input().split()
 n_peca2,quant2,v_peca2 = input().split()
 n_peca1 = int(n_peca1)
